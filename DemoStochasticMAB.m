f5ccb423e9e0f7202277e9b8ddf07d729fa972e7
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-10 20:52:55 +0200

    Add files via upload

--- DemoStochasticMAB.m
+++ DemoStochasticMAB.m
@@ -10,29 +10,30 @@ Horizon = 1000; %Overall number of interaction with the environment
 %----------------------------------------------------------
 
 alpha0 = 1; beta0 = 1;
-%[~, ArmsPlayedTS] = ThompsonSampling(environment, Horizon, alpha0, beta0);
+[~, ArmsPlayedTS] = ThompsonSampling(environment, Horizon, alpha0, beta0);
 
-%[~, ArmsPlayedUCB1] = UCB1(environment, Horizon);
-%[~, ArmsPlayedUCBV] = UCBV(environment, Horizon);
-%[~,ArmsPlayedBayesUCB] = BayesUCB(environment, Horizon, alpha0, beta0);
+[~, ArmsPlayedUCB1] = UCB1(environment, Horizon);
+[~, ArmsPlayedUCBV] = UCBV(environment, Horizon);
+[~,ArmsPlayedBayesUCB] = BayesUCB(environment, Horizon, alpha0, beta0);
 variant = 0;
 c = 1.01;
-%[~, ArmsPlayedKLUCB] = KLUCB(environment, Horizon, variant);
-%[~, ArmsPlayedCPUCB] = CPUCB(environment, Horizon, c);
+[~, ArmsPlayedKLUCB] = KLUCB(environment, Horizon, variant);
+[~, ArmsPlayedCPUCB] = CPUCB(environment, Horizon, c);
 [~, ArmsPlayedMOSS] = MOSS(environment, Horizon);
+
 %-----------------------------------------------------------
 %%             Plotting the results
 %-----------------------------------------------------------
-%plottingStochasticRegret(environment,ArmsPlayedTS,'Thompson Sampling');
+plottingStochasticRegret(environment,ArmsPlayedTS,'Thompson Sampling');
 
-%plottingStochasticRegret(environment,ArmsPlayedUCB1,'UCB 1');
+plottingStochasticRegret(environment,ArmsPlayedUCB1,'UCB 1');
 
-%plottingStochasticRegret(environment,ArmsPlayedUCBV,'UCB V');
+plottingStochasticRegret(environment,ArmsPlayedUCBV,'UCB V');
 
-%plottingStochasticRegret(environment,ArmsPlayedBayesUCB,'BayesUCB');
+plottingStochasticRegret(environment,ArmsPlayedBayesUCB,'BayesUCB');
 
-%plottingStochasticRegret(environment,ArmsPlayedKLUCB,'KLUCB');
+plottingStochasticRegret(environment,ArmsPlayedKLUCB,'KLUCB');
 
-%plottingStochasticRegret(environment,ArmsPlayedCPUCB,'CPUCB');
+plottingStochasticRegret(environment,ArmsPlayedCPUCB,'CPUCB');
 
 plottingStochasticRegret(environment,ArmsPlayedMOSS,'MOSS');
