e0826293b72f0cf0c38c9e0aa2c8d5e8aeb536c4
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-15 23:46:50 +0200

    Delete KLUCB_Initialize.m

--- KLUCB_Initialize.m
+++
@@ -1,6 +0,0 @@
-function [ExpectedMeans, NbrPlayArm, gainKLUCB, ArmsPlayed]= KLUCB_Initialize(K)
-    gainKLUCB = [];
-    ExpectedMeans = zeros(1,K);
-    NbrPlayArm = ones(1,K);
-    ArmsPlayed = [];
-end
