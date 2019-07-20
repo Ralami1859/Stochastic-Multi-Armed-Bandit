1bc174a8bec8b7e0081d005a80f9e06fcba8c832
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-20 23:00:21 +0200

    Delete UCBV_Initialize.m

--- UCBV_Initialize.m
+++
@@ -1,7 +0,0 @@
-function [ExpectedMeans, ExpectedVariances, NbrPlayArm, gainUCBV, ArmsPlayed]= UCBV_Initialize(K)
-    gainUCBV = [];
-    ExpectedMeans = zeros(1,K);
-    ExpectedVariances = zeros(1,K);
-    NbrPlayArm = ones(1,K);
-    ArmsPlayed = [];
-end
