03dff2afc5dbc12cb68144e43d0c11634db6b780
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-20 15:38:38 +0200

    Add files via upload

--- MOSS_RecommendArm.m
+++ MOSS_RecommendArm.m
@@ -1,5 +1,5 @@
 function [ArmToPlay]= MOSS_RecommendArm(ExpectedMeans, NbrPlayArm, Horizon)
-    ucb =  ExpectedMeans + sqrt(log(max(Horizon./(length(NbrPlayArm).*NbrPlayArm)))./NbrPlayArm);
+    ucb =  ExpectedMeans + sqrt(max(log(Horizon./(length(NbrPlayArm).*NbrPlayArm)),0)./NbrPlayArm);
     m = max(ucb); 
     I = find(ucb == m);
     ArmToPlay = I(1+floor(length(I)*rand));
