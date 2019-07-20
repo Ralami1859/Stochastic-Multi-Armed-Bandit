ea2dff0dea9785a22403f2a262ff768c28cce711
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-20 23:02:02 +0200

    Delete UCBV_RecommendArm.m

--- UCBV_RecommendArm.m
+++
@@ -1,9 +0,0 @@
-function [ArmToPlay]= UCBV_RecommendArm(ExpectedMeans, ExpectedVariances, NbrPlayArm, t)
-    if( t <=0)
-        error('Time step invalid. t<=0')
-    end
-    ucb = ExpectedMeans + sqrt(2*log(t).*ExpectedVariances./NbrPlayArm) + 3*log(t)./NbrPlayArm;
-    m = max(ucb);
-    I = find(ucb == m);
-    ArmToPlay = I(1+floor(length(I)*rand)); % Breaking randomly the tie
-end
