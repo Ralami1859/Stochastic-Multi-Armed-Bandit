7f4e3c137e7f34c84a71abc7f44c8ece8ff2cc91
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-20 22:59:53 +0200

    Delete MOSS_RecommendArm.m

--- MOSS_RecommendArm.m
+++
@@ -1,6 +0,0 @@
-function [ArmToPlay]= MOSS_RecommendArm(ExpectedMeans, NbrPlayArm, Horizon)
-    ucb =  ExpectedMeans + sqrt(max(log(Horizon./(length(NbrPlayArm).*NbrPlayArm)),0)./NbrPlayArm);
-    m = max(ucb); 
-    I = find(ucb == m);
-    ArmToPlay = I(1+floor(length(I)*rand));
-end
