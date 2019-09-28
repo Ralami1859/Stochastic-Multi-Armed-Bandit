6ff8870981380b6a39408b839a094617195a98aa
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-09-28 16:50:18 +0200

    Add files via upload

--- Algorithms/DMED.m
+++ Algorithms/DMED.m
@@ -1,18 +1,18 @@
-function [gainUCBV, ArmsPlayed] = UCBV(environment, Horizon)
+function [gainDMED, ArmsPlayed] = DMED(environment, Horizon, genuine)
 %addpath('../Modules/');   
 K = length(environment);
 %-------------------------------------------------------------------------------------------------------------
 %%                                                INITIALIZATION
 %-------------------------------------------------------------------------------------------------------------
 
-[Successes, Successes2, NbrPlayArm, gainUCBV, ArmsPlayed]= UCBV_Initialize(K);
+[ExpectedMeans, NbrPlayArm, gainDMED, ArmsPlayed, L]= DMED_Initialize(K);
 
 %---------------------------------------------------------------------------------------------------
 %%                                            INTERACTION
 %--------------------------------------------------------------------------------------------------
 
 for t = 1:Horizon;
-    ArmToPlay = UCBV_RecommendArm(Successes, Successes2, NbrPlayArm, t); % Arm recommended by UCBV
+    [ArmToPlay, L] = DMED_RecommendArm(ExpectedMeans, NbrPlayArm, t, L, genuine); % Arm recommended by UCB1
     reward = rand() < environment(ArmToPlay); % Reward received by playing the chosen arm
-    [Successes,Successes2, NbrPlayArm, gainUCBV, ArmsPlayed]= UCBV_ReceiveReward(Successes, Successes2, NbrPlayArm, reward, ArmToPlay, gainUCBV, ArmsPlayed); % Update UCB parameters using the reward received at time t.
+    [ExpectedMeans, NbrPlayArm, gainDMED, ArmsPlayed]= UCB1_ReceiveReward(ExpectedMeans, NbrPlayArm, reward, ArmToPlay, gainDMED, ArmsPlayed); % Update UCB parameters using the reward received at time t.
 end
